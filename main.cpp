59d6f39ea1801b32312f816653264c655f01cbb9
AlCatSplat <alcatsplat@gmail.com>
2023-01-25 18:43:52 -0800

    Version 0.1

--- main.cpp
+++ main.cpp
@@ -1,31 +1,54 @@
+//A pong-inspired game made in SFML by Alex Kibret. Copyright 2023, All Rights Reserved.
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 #include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Sample window");
-	//window.setVerticalSyncEnabled(true);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFMLPong");
+	window.setVerticalSyncEnabled(false);
 
 	bool paused = false;
+	bool restartGame = false;
+	bool lost = false;
+
+	float ballAngle = 75.f;
+	float ballSpeed = -400.f;
+
+	int playerScore = 0;
+
+	sf::Clock clock;
+
+	sf::Vector2f velocity;
 
 	sf::Font font;
 	if (!font.loadFromFile("oswald.ttf")) {
 		std::cout << "Error loading font!";
 	}
 
-	int playerScore = 0;
-
 	sf::Text text;
 	text.setFont(font);
-	text.setString("Sus amongus!, Fortnite balls!!!");
+	text.setString("Single player mode");
 	text.setCharacterSize(24);
 	text.setFillColor(sf::Color::Blue);
 
 	sf::Text score;
 	score.setFont(font);
 	score.setCharacterSize(24);
-	score.setPosition(200.f, 50.f);
+	score.setPosition(300.f, 50.f);
+
+	sf::Text youLose;
+	youLose.setFont(font);
+	youLose.setString("You lose!");
+	youLose.setCharacterSize(72);
+	youLose.setPosition(260.f, 200.f);
+
+	sf::Text restart;
+	restart.setFont(font);
+	restart.setString("Press R to restart");
+	restart.setCharacterSize(30);
+	restart.setPosition(280.f, 300.f);
 
 	sf::RectangleShape player(sf::Vector2f(10.f, 120.f));
 	player.setPosition(10.f, 50.f);
@@ -35,86 +58,150 @@ int main()
 
 	sf::RectangleShape ball(sf::Vector2f(20.f, 20.f));
 	ball.setPosition(700.f, 200.f);
-	float ballSpeed = -0.12;
-	bool ballHitPlayer = false;
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			bool focus = window.hasFocus();
+	sf::RectangleShape topBound(sf::Vector2f(800.f, 1.f));
+	topBound.setPosition(0.f, 0.f);
+	topBound.setFillColor(sf::Color::Cyan);
 
-			if (focus == false)
-			{
-				window.setTitle("paused");
-				paused = true;
-			}
-			else
-			{
-				window.setTitle("Sample window");
-				paused = false;
-			}
+	sf::RectangleShape bottomBound(sf::Vector2f(800.f, 1.f));
+	bottomBound.setPosition(0.f, 599.f);
+	bottomBound.setFillColor(sf::Color::Cyan);
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-			{
-				player.move(0.f, -10.f);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-			{
-				player.move(0.f, 10.f);
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-			{
-				window.close();
-			}
+	sf::RectangleShape leftBound(sf::Vector2f(1.f, 600.f));
+	leftBound.setPosition(10.f, 0.f);
+	leftBound.setFillColor(sf::Color::Cyan);
 
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
-
-		window.clear(sf::Color::Black);
-
-		window.draw(text);
-		window.draw(score);
-		window.draw(player);
-		window.draw(wall);
-		window.draw(ball);
-
-		window.display();
-
-		if (paused == false)
-		{
-			if (ballHitPlayer == false)
-			{
-				ball.move(ballSpeed, 0.f);
-			}
-
-			sf::FloatRect ballBounds = ball.getGlobalBounds();
-			sf::FloatRect playerBounds = player.getGlobalBounds();
-			sf::FloatRect wallBounds = wall.getGlobalBounds();
-			if (ballBounds.intersects(playerBounds))
-			{
-				ballHitPlayer = true;
-			}
-			if (ballHitPlayer)
-			{
-				ball.move(-ballSpeed, 0.f);
-			}
-
-			if (ballBounds.intersects(wallBounds))
-			{
-				ballHitPlayer = false;
-				playerScore++;
-				std::cout << playerScore;
-			}
-
-			auto s = std::to_string(playerScore);
-			score.setString("Current score: " + s);
-		}
+	sf::SoundBuffer pingBuffer;
+	if (!pingBuffer.loadFromFile("ping.wav")) {
+		std::cout << "Error loading sound!";
 	}
 
+	sf::SoundBuffer deathBuffer;
+	if (!deathBuffer.loadFromFile("death.wav")) {
+		std::cout << "Error loading sound!";
+	}
+
+	sf::SoundBuffer edgeBuffer;
+	if (!edgeBuffer.loadFromFile("edge.wav")) {
+		std::cout << "Error loading sound!";
+	}
+
+	sf::Sound ping;
+	ping.setBuffer(pingBuffer);
+
+	sf::Sound death;
+	death.setBuffer(deathBuffer);
+
+	sf::Sound edge;
+	edge.setBuffer(edgeBuffer);
+
+	do {
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				bool focus = window.hasFocus();
+
+				if (focus == false) {
+					window.setTitle("paused");
+					paused = true;
+				}
+				else {
+					window.setTitle("SFMLPong");
+					paused = false;
+				}
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+					player.move(0.f, -10.f);
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+					player.move(0.f, 10.f);
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+					window.close();
+				}
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::R) && lost) {
+					restartGame = true;
+				}
+
+				if (event.type == sf::Event::Closed) {
+					window.close();
+				}
+			}
+
+			window.clear(sf::Color::Black);
+
+			window.draw(text);
+			window.draw(score);
+			window.draw(player);
+			window.draw(wall);
+			window.draw(ball);
+			window.draw(topBound);
+			window.draw(bottomBound);
+			window.draw(leftBound);
+
+			if (!paused)
+			{
+				float deltaTime = clock.restart().asSeconds();
+				float factor = deltaTime * ballSpeed;
+				velocity.x = std::cos(ballAngle) * factor;
+				velocity.y = std::sin(ballAngle) * factor;
+
+				ball.move(velocity.x, velocity.y);
+
+				if (ball.getGlobalBounds().intersects(player.getGlobalBounds())) {
+					ball.move(-velocity.x, -velocity.y);
+					velocity.x = -velocity.x;
+					ballAngle = -ballAngle;
+					ballSpeed = -ballSpeed;
+					std::cout << "ping";
+					ping.play();
+				}
+
+				if (ball.getGlobalBounds().intersects(wall.getGlobalBounds())) {
+					ball.move(-velocity.x, -velocity.y);
+					velocity.x = -velocity.x;
+					ballAngle = -ballAngle;
+					ballSpeed = -ballSpeed;
+					playerScore++;
+					std::cout << "pong";
+					ping.play();
+				}
+
+				if (ball.getGlobalBounds().intersects(topBound.getGlobalBounds())) {
+					velocity.y = -velocity.y;
+					ballAngle = -ballAngle;
+					edge.play();
+				}
+
+				if (ball.getGlobalBounds().intersects(bottomBound.getGlobalBounds())) {
+					velocity.y = -velocity.y;
+					ballAngle = -ballAngle;
+					edge.play();
+				}
+
+				if (ball.getGlobalBounds().intersects(leftBound.getGlobalBounds())) {
+					lost = true;
+					death.play();
+				}
+
+				if (lost) {
+					window.draw(youLose);
+					window.draw(restart);
+				}
+
+				if (restartGame) {
+					std::cout << "poop";
+				}
+
+				window.display();
+
+				auto s = std::to_string(playerScore);
+				score.setString("Current score: " + s);
+
+			}
+		}
+	} while (!restartGame);
 	return 0;
 }
